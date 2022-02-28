8ec558571cf3ecf536c1cda09d932d71b9f0a6b8
WilliamB-EPS <wbowers@eastsideprep.org>
2022-02-28 10:10:31 -0800

    push

---
+++ final-project/js/script.js
@@ -0,0 +1,11 @@
+// functions for nav bar animation:
+
+function openNav() {
+    document.getElementById("nav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("nav").style.height = "0%";
+}
+
+// functions for slide show (html on portfolio page):
