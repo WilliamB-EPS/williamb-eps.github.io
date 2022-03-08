290c1dbd9d7a6d887d4e4d017d7150a5624f339e
WilliamB-EPS <wbowers@eastsideprep.org>
2022-03-07 20:06:07 -0800

    push

--- space-fm-project/js/script.js
+++ space-fm-project/js/script.js
@@ -6,7 +6,7 @@ var day = today.getDay();
 var show;
 // monday
 if (day == 1) {
-    if (19 <= time <= 21) {
+    if (19 <= time && time <= 21) {
         show = "Roots Roundup";
     } else {
         show = "SPACE MIX ";
@@ -93,25 +93,4 @@ if (day == 1) {
     } 
     } else {
         show = "SPACE MIX";
-}
-
-function httpGet(theUrl)
-{
-    if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp=new XMLHttpRequest();
-    }
-    else
-    {// code for IE6, IE5
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange=function()
-    {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        {
-            return xmlhttp.responseText;
-        }
-    }
-    xmlhttp.open("GET", theUrl, false );
-    xmlhttp.send();    
 }
